94acbbb5d056436069c6cf0e85d191fc9b90417d
danielHLopes <danielhlopes90@gmail.com>
2020-05-27 17:34:45 -0400

    Paginação dos alunos tela monitor pt2

--- Frontend/src/pages/monitor/index.js
+++ Frontend/src/pages/monitor/index.js
@@ -1,13 +1,13 @@
 ﻿import React,{ useState } from 'react';
-import { Link, useHistory } from 'react-router-dom';  
+import { Link, useHistory } from 'react-router-dom';
 
 import './styles.css';
 import setaDImg from '../../assets/setaD.png';
 import setaEImg from '../../assets/setaE.png';
- 
+import alunoImg from '../../assets/aluno.png';
 export default function Monitor()
-{ 
-    
+{
+
     const history = useHistory();
 
     async function proxAluno(e) {
@@ -34,12 +34,27 @@ export default function Monitor()
         <div>
             <nav>
                 <ul class="paginacao">
-                    <li class="paginacao"><button type="submit"><img className="seta" src={setaDImg}></img></button></li>
-                    <li class="page-item"><button type="submit"><img className="seta" src={setaEImg}></img></button></li>
+                    <li class="paginacao">
+                        <form onSubmit={proxAluno}>
+                            <button type="submit">
+                                <img className="seta" src={setaDImg}></img>
+                            </button>
+                        </form>
+                    </li>
+                    <div>
+                        <img src={alunoImg}></img>
+                        <p>Nome aluno</p>
+                    </div>
+                    <li class="page-item">
+                        <form onSubmit={proxAluno}>
+                            <button type="submit">
+                                <img className="seta" src={setaEImg}></img>
+                            </button>
+                        </form>
+                    </li>
                 </ul>
             </nav>
         </div>
-            
         </div>
     );
 }
