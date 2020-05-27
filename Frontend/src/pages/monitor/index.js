eb6c121d978feaf47823e02ff8e9462c8d0b5e7d
danielHLopes <danielhlopes90@gmail.com>
2020-05-27 15:22:21 -0400

    Paginação dos alunos tela monitor pt1

--- Frontend/src/pages/monitor/index.js
+++ Frontend/src/pages/monitor/index.js
@@ -1,12 +1,45 @@
-﻿import React,{ useState } from 'react';  
+﻿import React,{ useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';  
 
 import './styles.css';
+import setaDImg from '../../assets/setaD.png';
+import setaEImg from '../../assets/setaE.png';
  
 export default function Monitor()
 { 
+    
+    const history = useHistory();
+
+    async function proxAluno(e) {
+        e.preventDefault();
+
+        try {
+            history.push('/monitor');
+        } catch (err) {
+            alert(err)
+        }
+    }
+    async function antAluno(e) {
+        e.preventDefault();
+
+        try {
+            history.push('/monitor');
+        } catch (err) {
+            alert(err)
+        }
+    }
     return(
         <div>
-            <p>Monitor</p>
+            <h1>Monitoria </h1>
+        <div>
+            <nav>
+                <ul class="paginacao">
+                    <li class="paginacao"><button type="submit"><img className="seta" src={setaDImg}></img></button></li>
+                    <li class="page-item"><button type="submit"><img className="seta" src={setaEImg}></img></button></li>
+                </ul>
+            </nav>
+        </div>
+            
         </div>
     );
 }
