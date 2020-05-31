14ad25270f39019ebcb4b32908563b1fa4fbfcb1
Gabriel <egitoo@gmail.com>
2020-05-31 17:01:53 -0400

    Criacao de rotas para turma e disciplina

---
+++ backend/src/controllers/turmaController.js
@@ -0,0 +1,37 @@
+const connection = require('../configs/connection');
+
+
+module.exports = {
+    async list(req, res) {
+
+        await connection('turma').select('*').then(result => {
+            return res.status(200).json(result);
+        }).catch(error => {
+            return res.status(500).json(error);
+        });
+
+    },
+
+    async listPorSerie(req, res) {
+        const { serie_id } = req.params;
+
+        await connection('turma').select('*').where('serie_id', serie_id || 0).then(result => {
+            return res.status(200).json(result);
+        }).catch(error => {
+            return res.status(500).json(error);
+        });
+    },
+
+    async create(req, res) {
+        const { nome, ano, serie_id } = req.body;
+
+        await connection('turma').insert({
+            nome, ano, serie_id
+        }).then(result => {
+            return res.status(200).json(result);
+        }).catch(erro => {
+            return res.status(500).json(erro);
+        });
+    }
+}
+
--- backend/src/routers/index.js
+++ backend/src/routers/index.js
@@ -3,7 +3,8 @@ const Routers = express.Router();
 
 const cursoController = require('../controllers/cursoController');
 const serieController = require('../controllers/serieController');
-
+const turmaController = require('../controllers/turmaController');
+const disciplinaController = require('../controllers/disciplinaController');
 
 Routers.get('/curso', cursoController.list);
 Routers.post('/curso', cursoController.create);
@@ -11,5 +12,14 @@ Routers.post('/curso', cursoController.create);
 Routers.get('/serie', serieController.list);
 Routers.post('/serie', serieController.create);
 Routers.get('/serie/:curso_id', serieController.listPorCurso)
+
+Routers.get('/turma', turmaController.list);
+Routers.post('/turma', turmaController.create);
+Routers.get('/turma/:serie_id', turmaController.listPorSerie);
+
+Routers.get('/disciplina', disciplinaController.list);
+Routers.post('/disciplina', disciplinaController.create);
+Routers.get('/disciplina/:serie_id', disciplinaController.listPorSerie);
+
 
 module.exports = Routers;
