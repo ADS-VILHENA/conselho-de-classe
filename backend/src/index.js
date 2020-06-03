61f841a64331c06da65d9b44f69d3433ed1cce26
WallysonMarcos <wallyson78@gmail.com>
2020-06-02 20:06:45 -0400

    Ajuste CORS

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,10 @@
 ﻿const express = require('express');
 const routes = require('./routers');
+const cors = require('cors');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
