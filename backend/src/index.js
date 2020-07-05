51a49ac89dcaa129d013e008afbc39fb2d888f77
WallysonMarcos <wallyson78@gmail.com>
2020-07-05 19:07:03 -0400

    ajuste index api

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 ﻿const express = require('express');
 const routes = require('./routers');
 const cors = require('cors');
+const port = (process.env.PORT || 3001);
 
 const app = express();
 
@@ -8,4 +9,6 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(process.env.PORT || 3001 );
+app.listen(port|| 3001 , () => {
+    console.log(`Servidor Rodando na Porta  ${port}`)
+});
