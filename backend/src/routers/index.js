23c028f156ef18b7795b5017f2980fb980c9a9b6
daniel.lopes <48608848+danielHLopes@users.noreply.github.com>
2020-07-07 22:16:08 -0400

    diagnostico por serie
    rota para puxar diagnostico por serie

--- backend/src/routers/index.js
+++ backend/src/routers/index.js
@@ -15,12 +15,13 @@ const diagnosticoController = require('../controllers/diagnosticoController');
 
 
 Routers.get('/diagnostico/turma', diagnosticoController.listTurma);
-Routers.get('/diagnostico/turma/:idPeriodo', diagnosticoController.listPorPeriodo)
+Routers.get('/diagnostico/turma/periodo/:idPeriodo', diagnosticoController.listPorPeriodo)
 Routers.post('/diagnostico/turma', diagnosticoController.createTurma);
 
 Routers.get('/diagnostico/aluno', diagnosticoController.listAluno);
 Routers.post('/diagnostico/aluno', diagnosticoController.createAluno);
-Routers.get('/diagnostico/aluno/filter', diagnosticoController.listPorAluno)
+Routers.get('/diagnostico/aluno/serie', diagnosticoController.listPorAluno)
+Routers.get('/diagnostico/serie', diagnosticoController.listPorSerie)
 
 Routers.get('/indice', indiceController.list);
 Routers.post('/indice', indiceController.create);
@@ -30,28 +31,28 @@ Routers.post('/curso', cursoController.create);
 
 Routers.get('/serie', serieController.list);
 Routers.post('/serie', serieController.create);
-Routers.get('/serie/:curso_id', serieController.listPorCurso)
+Routers.get('/serie/curso/:curso_id', serieController.listPorCurso)
 
 Routers.get('/turma', turmaController.list);
 Routers.post('/turma', turmaController.create);
-Routers.get('/turma/:serie_id', turmaController.listPorSerie);
+Routers.get('/turma/serie/:serie_id', turmaController.listPorSerie);
 
 Routers.get('/disciplina', disciplinaController.list);
 Routers.post('/disciplina', disciplinaController.create);
-Routers.get('/disciplina/:serie_id', disciplinaController.listPorSerie);
+Routers.get('/disciplina/serie/:serie_id', disciplinaController.listPorSerie);
 
 Routers.get('/aluno', alunoController.list);
 Routers.post('/aluno', alunoController.create);
-Routers.get('/aluno/:serie_id', alunoController.listNotas);
+Routers.get('/aluno/serie/:serie_id', alunoController.listNotas);
 
 
 Routers.get('/matricula', matriculaController.list);
 Routers.post('/matricula', matriculaController.create);
-Routers.get('/matricula/:disciplina_id', matriculaController.listPorDisciplina);
+Routers.get('/matricula/disciplina/:disciplina_id', matriculaController.listPorDisciplina);
 
 Routers.get('/periodo', periodoController.list);
 Routers.post('/periodo', periodoController.create);
-Routers.get('/periodo/:serie_id', periodoController.listPorSerie)
+Routers.get('/periodo/serie/:serie_id', periodoController.listPorSerie)
 
 Routers.get('/perfil_turma', perfilTurmaController.list);
 
