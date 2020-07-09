e484bafbfb962d062c14b1eb6c027f7bba08636e
WallysonMarcos <wallyson78@gmail.com>
2020-07-08 23:34:20 -0400

    baseURL axios

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-   // baseURL:'https://coselhodeclasse-api.herokuapp.com/'
-   baseURL:'http://localhost:3001/'
+   baseURL:'https://coselhodeclasse-api.herokuapp.com/'
+   //baseURL:'http://localhost:3001/'
 });
 
 export default api ;
